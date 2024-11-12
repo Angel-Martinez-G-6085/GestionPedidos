8dc398e8bcb6a6da9092950068b127c5eab7a4a6
Angel Ivan Martinez Garrido <ben6085@gmail.com>
2024-11-11 20:46:49 -0600

    Se inicio el proyecto del microservicio de gestion de pedidos

---
+++ src/test/java/com/codecave/blog/GestionPedidosApi/GestionPedidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecave.blog.GestionPedidosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPedidosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
